8c2a76a720e52cb089e3103cce4d879719390bae
manufebie <manu.febie@gmail.com>
2018-10-10 13:59:07 +0700

    Fixed AttributeError. Forgot to let the Ship class inherit from Spite. Finished chap14

--- ship.py
+++ ship.py
@@ -3,7 +3,7 @@ import pygame
 from pygame.sprite import Sprite
 
 
-class Ship:
+class Ship(Sprite):
 
     def __init__(self, ai_settings, screen):
         '''Initialize the ship and set its starting position.'''
